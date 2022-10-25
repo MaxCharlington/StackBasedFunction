8ae72092ee582396ea7927aba29baf420bf1e029
Max Charlington <36207528+MaxCharlington@users.noreply.github.com>
2022-10-25 17:51:35 +0300

    Added function pointer support
    No support for call operator for function pointer constructed function is provided yet

--- stack_based_function.cpp
+++ stack_based_function.cpp
@@ -3,31 +3,25 @@
 #include <cstring>
 #include <iostream>
 
-// std::function replacement to store lambda of any size on stack
+/* std::function replacement to store lambda of any size on stack*/
 template <size_t CtxSize, size_t Align, typename Ret, typename ...Args>
 class function {
     alignas(Align) std::array<std::byte, CtxSize> ctx;  // lambda capture
-    Ret(*func)(void*);
+    Ret(*func)(void*, Args...);
 
 public:
     template <typename Lambda>
     function(Lambda&& lmb)
     {
-        if constexpr (CtxSize == 0)
-        {
-            func = [ptr = static_cast<Ret(*)(Args...)>(lmb)]([[maybe_unused]] void*, Args ...args){ return ptr(args...); };
-        }
-        else 
-        {
-            func = [](void* ctx, Args... args) { return (*static_cast<Lambda*>(ctx))(args...); };
-            new (ctx.data()) Lambda{std::forward<Lambda>(lmb)};
-        }
+        func = [](void* ctx, Args... args) { return (*static_cast<Lambda*>(ctx))(args...); };
+        new (ctx.data()) Lambda{std::forward<Lambda>(lmb)};
     }
     
     template <typename Ret_, typename ...Args_>
-    function(Ret_(*lmb)(Args_...))
+    function(Ret_(*ptr)(Args_...))
     {
-        func = [lmb]([[maybe_unused]] void*, Args_ ...args){ return lmb(args...); };
+        func = [](void* ctx, Args ...args){ return (reinterpret_cast<Ret(*)(Args...)>(ctx))(args...); };
+        new (ctx.data()) decltype(ptr){ptr};
     }
 
     template <typename Lambda>
@@ -36,42 +30,35 @@ public:
         static_assert(alignof(Lambda) == Align);
         static_assert(std::is_same_v<decltype(lmb()), Ret>);
 
-        if constexpr (CtxSize == 0)
-        {
-            func = [ptr = static_cast<Ret(*)(Args...)>(lmb)]([[maybe_unused]] void*, Args ...args){ return ptr(args...); };
-        }
-        else 
-        {
-            func = [](void* ctx, Args... args) { return (*static_cast<Lambda*>(ctx))(args...); };
-            new (ctx.data()) Lambda{std::forward<Lambda>(lmb)};
-        }
+        func = [](void* ctx, Args... args) { return (*static_cast<Lambda*>(ctx))(args...); };
+        new (ctx.data()) Lambda{std::forward<Lambda>(lmb)};
         return *this;
     }
 
     template <typename Ret_, typename ...Args_>
-    function& operator=(Ret_(*lmb)(Args_...))
+    function& operator=(Ret_(*ptr)(Args_...))
     {
-        func = [lmb]([[maybe_unused]] void*, Args_ ...args){ return lmb(args...); };
+        static_assert(std::is_same_v<Ret_, Ret>);
+        static_assert(std::is_same_v<std::tuple<Args_...>, std::tuple<Args...>>);
+
+        func = [](void* ctx, Args ...args){ return (reinterpret_cast<Ret(*)(Args...)>(ctx))(args...); };
+        new (ctx.data()) decltype(ptr){ptr};
     }
 
-    // function() {
-    //     func = nullptr;
-    // }
+    function() {
+        func = nullptr;
+    }
 
     function(const function&) = default;
     function(function&&) noexcept = default;
     function& operator=(const function&) = default;
     function& operator=(function&&) = default;
 
-    decltype(auto) operator()() {
-        if constexpr (CtxSize == 0)
-        {
-            return func();
-        }
-        else
-        {
-            return func(ctx.data());
-        }
+    template<typename ...Args_>
+    Ret operator()(Args_... args) {
+        static_assert(std::is_same_v<std::tuple<Args_...>, std::tuple<Args...>>);
+    
+        return func(ctx.data(), args...);
     }
 
     operator bool() {
@@ -84,16 +71,24 @@ function(Lambda&& lmb) ->
     function<sizeof(Lambda), alignof(Lambda), decltype(lmb())>;
 
 template <typename Ret_, typename ...Args_>
-function(Ret_(*lmb)(Args_...)) ->
-    function<0, 0, Ret_, Args_...>;
+function(Ret_(*ptr)(Args_...)) ->
+    function<sizeof(ptr), alignof(Ret_(*)(Args_...)), Ret_, Args_...>;
 
-int f(int) { return 0; }
+
+int foo(int) { return 0; }
 
 int main()
 {
     function f{[a=1, b=10](){}};
     f = [f=5.6f, g=1](){};
 
+    f();
+
     function f2{[](){}};
-    // function f3{&f};
+
+    f2();
+
+    function f3{&foo};
+
+    // auto a = f3(1);
 }
